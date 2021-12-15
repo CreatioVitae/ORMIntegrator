fa41c071486582d5f4842135fdc03ec808cfbe14
hchiba1984 <h.chiba201103@gmail.com>
2021-12-15 23:53:50 +0900

    Update ServiceCollectionExtensionLibrary.cs

--- src/ORMIntegrator.Extensions.DependencyInjection/ServiceCollectionExtensionLibrary.cs
+++ src/ORMIntegrator.Extensions.DependencyInjection/ServiceCollectionExtensionLibrary.cs
@@ -4,7 +4,7 @@ using System;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
-public static class ServiceCollectionExtensionLibrary {
+public static class ServiceCollectionExtensionLibrary      {
     public static IServiceCollection AddSqlManager<TDbContext>(this IServiceCollection serviceDescriptors, Func<string, TDbContext> dbContextFactoryMethod, string connectionString) where TDbContext : DbContext, new() {
         serviceDescriptors.AddScoped(_ =>
             new SqlManager<TDbContext>(
