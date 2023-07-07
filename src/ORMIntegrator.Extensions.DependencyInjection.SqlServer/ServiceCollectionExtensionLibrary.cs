443e32a70f6dc86019de6c940a9680a3e7ad342c
hchiba1984 <h.chiba201103@gmail.com>
2023-07-07 22:02:26 +0900

    upgrade package.

--- src/ORMIntegrator.Extensions.DependencyInjection.SqlServer/ServiceCollectionExtensionLibrary.cs
+++ src/ORMIntegrator.Extensions.DependencyInjection.SqlServer/ServiceCollectionExtensionLibrary.cs
@@ -9,16 +9,16 @@ public static class ServiceCollectionExtensionLibrary {
     public static IServiceCollection AddSqlManager<TDbContext>(this IServiceCollection serviceDescriptors, Func<string, bool, TDbContext> dbContextFactoryMethod, IConfiguration configuration, string dbContextName, IDefaultEnvironmentAccessor defaultEnvironment, bool isHandleAsSingleton = false) where TDbContext : DbContext, new() {
         var databaseConfig = configuration.GetSection(DatabaseConfig.GetDefaultSection(dbContextName)).GetAvailable<DatabaseConfig>();
 
-        return AddSqlManagerLocal(serviceDescriptors, dbContextFactoryMethod, defaultEnvironment, isHandleAsSingleton, databaseConfig);
+        return AddSqlManagerLocal(serviceDescriptors, dbContextFactoryMethod, defaultEnvironment, isHandleAsSingleton, databaseConfig, configuration);
     }
 
     public static IServiceCollection AddSqlManager<TDbContext>(this IServiceCollection serviceDescriptors, Func<string, bool, TDbContext> dbContextFactoryMethod, (IConfiguration configuration, string sectionName) configurationAndSectionName, IDefaultEnvironmentAccessor defaultEnvironment, bool isHandleAsSingleton = false) where TDbContext : DbContext, new() {
         var databaseConfig = configurationAndSectionName.configuration.GetSection(configurationAndSectionName.sectionName).GetAvailable<DatabaseConfig>();
 
-        return AddSqlManagerLocal(serviceDescriptors, dbContextFactoryMethod, defaultEnvironment, isHandleAsSingleton, databaseConfig);
+        return AddSqlManagerLocal(serviceDescriptors, dbContextFactoryMethod, defaultEnvironment, isHandleAsSingleton, databaseConfig, configurationAndSectionName.configuration);
     }
 
-    static IServiceCollection AddSqlManagerLocal<TDbContext>(IServiceCollection serviceDescriptors, Func<string, bool, TDbContext> dbContextFactoryMethod, IDefaultEnvironmentAccessor defaultEnvironment, bool isHandleAsSingleton, DatabaseConfig databaseConfig) where TDbContext : DbContext, new() {
+    static IServiceCollection AddSqlManagerLocal<TDbContext>(IServiceCollection serviceDescriptors, Func<string, bool, TDbContext> dbContextFactoryMethod, IDefaultEnvironmentAccessor defaultEnvironment, bool isHandleAsSingleton, DatabaseConfig databaseConfig, IConfiguration configuration) where TDbContext : DbContext, new() {
         var sqlConnectionStringBuilder = new SqlConnectionStringBuilder {
             ConnectionString = databaseConfig.ConnectionString,
             ApplicationName = databaseConfig.ApplicationName
@@ -32,6 +32,6 @@ public static class ServiceCollectionExtensionLibrary {
             sqlConnectionStringBuilder.MaxPoolSize = databaseConfig.MaxPoolSize.Value;
         }
 
-        return serviceDescriptors.AddSqlManager(dbContextFactoryMethod, sqlConnectionStringBuilder.ConnectionString, defaultEnvironment, isHandleAsSingleton);
+        return serviceDescriptors.AddSqlManager(dbContextFactoryMethod, sqlConnectionStringBuilder.ConnectionString, configuration, defaultEnvironment, isHandleAsSingleton);
     }
 }
